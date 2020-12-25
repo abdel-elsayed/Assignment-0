b2411e54133a05b356f1a42eb478f8a6026057fa
abdel-elsayed <abdel.elsayed18@gmail.com>
2020-12-25 15:54:13 -0500

    Solved problems 00-06

--- 06-countOfAllNumbersSmallerThanTarget/countOfAllNumbersSmallerThanTarget.js
+++ 06-countOfAllNumbersSmallerThanTarget/countOfAllNumbersSmallerThanTarget.js
@@ -1,5 +1,12 @@
 function countOfAllNumbersSmallerThanTarget(nums, target) {
   // Insert code here;
+  nums.sort();
+  count = 0;
+
+  while(nums[count] < target){
+    count++;  
+  }
+  return count;
 }
 
 // Do not edit this line;
